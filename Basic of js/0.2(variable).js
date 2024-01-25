951c0727de81baad0bfabaf4db0902a4853140ae
anuescapist <annuu088@gmail.com>
2024-01-25 16:15:17 +0530

    second commit

---
+++ Basic of js/0.2(variable).js
@@ -0,0 +1,15 @@
+// introduction to variable 
+
+// variable  basicly is a type of container where we keep our value we can change information  of that variable later
+// in js we can declare variable in three type 
+// let = in let we need to give value when we are declaring it 
+// const = in const we  cant change the value later 
+// var = in var we can declare it without  declaring value
+
+let a= 7;
+console.log(a);
+//it will give error if we dont assign value to it 
+var b;
+const c = 67;
+var b = a+c;
+console.log(b);
