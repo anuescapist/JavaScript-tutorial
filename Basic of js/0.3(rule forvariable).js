1b88bed40531393da3c95e21cf19092979218eaa
anuescapist <annuu088@gmail.com>
2024-01-25 22:54:08 +0530

    third commit

---
+++ Basic of js/0.3(rule forvariable).js
@@ -0,0 +1,8 @@
+// you can't start with var 
+//  you cant start with number
+// you can put number after name of variable
+var a= 2;
+console.log( a **3);
+// here a is our variable ** for  3 her for number of times we want it power 
+console.log(a ** 0.5);
+// squareroot for var a
